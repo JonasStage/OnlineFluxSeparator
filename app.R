704804fec87fa33bb374a2098e4c3ceb0e70c295
JonasStage <Jonasstage@hotmail.com>
2024-06-10 17:00:32 -0700

    Added features to allow for upload of data without (0) CO2 values.

--- app.R
+++ app.R
@@ -145,6 +145,7 @@ server <- function(input, output, session){
                       min = time_start, max = time_end, step = 60)
     
     return(df)
+
     
   })
   
@@ -163,28 +164,38 @@ server <- function(input, output, session){
          xlab="Datetime",
          main = "Overview plot",
          col = "darkorange")
+   
+     data() %>% 
+        reframe(across(co2, c(mean = mean,var = var))) -> co2_status
     
-    mtext(expression("CO"[2]*" (ppm)"), side = 4, line = 3, col="forestgreen")
-
-    co2_min = min(data_subset$co2)
-    co2_max = max(data_subset$co2)
-    ch4_min = min(data_subset$ch4)
-    ch4_max = max(data_subset$ch4)
+    if (co2_status$co2_mean == 0 & co2_status$co2_var == 0) {
+      legend("topright", 
+             c(expression("CH"[4])), 
+             col = "darkorange", pch=19)
+      } else {
     
-    ch4_scaled = (co2_max - co2_min)*((data_subset$ch4-ch4_min)/(ch4_max - ch4_min))+co2_min
-    ch4_labels = pretty(data_subset$ch4)
-    ch4_at = (co2_max - co2_min)*((ch4_labels-ch4_min)/(ch4_max - ch4_min))+co2_min
-    
-    co2_scaled = (ch4_max - ch4_min)*((data_subset$co2-co2_min)/(co2_max - co2_min))+ch4_min
-    co2_labels = pretty(data_subset$co2)
-    co2_at = (ch4_max - ch4_min)*((co2_labels-co2_min)/(co2_max - co2_min))+ch4_min
-    
-    points(x = data_subset$datetime, y = co2_scaled, col="forestgreen")
-    axis(4, at = co2_at, labels = co2_labels, col="forestgreen", col.ticks="forestgreen")
-    
-    legend("topright", 
-           c(expression("CH"[4]), expression("CO"[2])), 
-           col = c("darkorange","forestgreen"), pch=19)
+      mtext(expression("CO"[2]*" (ppm)"), side = 4, line = 3, col="forestgreen")
+  
+      co2_min = min(data_subset$co2)
+      co2_max = max(data_subset$co2)
+      ch4_min = min(data_subset$ch4)
+      ch4_max = max(data_subset$ch4)
+      
+      ch4_scaled = (co2_max - co2_min)*((data_subset$ch4-ch4_min)/(ch4_max - ch4_min))+co2_min
+      ch4_labels = pretty(data_subset$ch4)
+      ch4_at = (co2_max - co2_min)*((ch4_labels-ch4_min)/(ch4_max - ch4_min))+co2_min
+      
+      co2_scaled = (ch4_max - ch4_min)*((data_subset$co2-co2_min)/(co2_max - co2_min))+ch4_min
+      co2_labels = pretty(data_subset$co2)
+      co2_at = (ch4_max - ch4_min)*((co2_labels-co2_min)/(co2_max - co2_min))+ch4_min
+      
+      points(x = data_subset$datetime, y = co2_scaled, col="forestgreen")
+      axis(4, at = co2_at, labels = co2_labels, col="forestgreen", col.ticks="forestgreen")
+      
+      legend("topright", 
+             c(expression("CH"[4]), expression("CO"[2])), 
+             col = c("darkorange","forestgreen"), pch=19)
+    }
     
   })
   
@@ -193,17 +204,15 @@ server <- function(input, output, session){
   data_subset <- reactive({
     req(input$file)
     
+    data() %>% 
+      reframe(across(co2, c(mean = mean,var = var))) -> co2_status
+    
     if (!is.null(ranges2$x)) {
       ranges2$x <- as_datetime(ranges2$x)
       
       data_subset <- data() %>%
         filter(between(datetime, ranges2$x[1], ranges2$x[2])) %>%
         mutate(sec = cumsum(c(0, diff(as.numeric(datetime))))) 
-      
-      lm_model_co2 <- lm(co2~sec, data = data_subset)
-      slope_co2 <- coef(lm_model_co2)[2]
-      intercept_co2 <- coef(lm_model_co2)[1]
-      r2_co2 <- summary(lm_model_co2)$r.squared
       
       lm_model_ch4 <- lm(ch4~sec, data = data_subset)
       slope_ch4 <- coef(lm_model_ch4)[2]
@@ -214,10 +223,41 @@ server <- function(input, output, session){
       
       R <- 0.08206 #L atm K^-1 mol^-1
       
-      co2_flux <- (slope_co2*(input$chamber_vol*input$atm_pres))/(R*(273.15+mean_temp)*input$chamber_area)
       ch4_flux <- (slope_ch4*(input$chamber_vol*input$atm_pres))/(R*(273.15+mean_temp)*input$chamber_area)
       
-      results_string <- paste0("<b>CH<sub>4</sub></b>: slope = ", round(slope_ch4*3600, 2), " (ppm h<sup>-1</sup>)",
+        legend("topright", 
+               c(expression("CH"[4])), 
+               col = "darkorange", pch=19)
+     
+      if (co2_status$co2_mean == 0 & co2_status$co2_var == 0) {
+        
+        
+        results_string <- paste0("<b>CH<sub>4</sub></b>: slope = ", round(slope_ch4*3600, 2), " (ppm h<sup>-1</sup>)",
+                                 ", flux = ", round(ch4_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)",
+                                 ", R<sup>2</sup> = ", round(r2_ch4, 2))
+        
+        results <- data.frame("processing_date" = strftime(Sys.time(), "%Y-%m-%d %H:%M:%S", tz="UTC"),
+                              "id" = as.character(input$sample_id),
+                              "start" = strftime(ranges2$x[1], "%Y-%m-%d %H:%M:%S", tz="UTC"),
+                              "end" = strftime(ranges2$x[2], "%Y-%m-%d %H:%M:%S", tz="UTC"),
+                              "CH4_slope" = slope_ch4*3600,
+                              "CH4_intercept" = intercept_ch4,
+                              "CH4_R2" = r2_ch4,
+                              "temperature" = mean_temp, 
+                              "chamber_volume" = input$chamber_vol,
+                              "chamber_area" = input$chamber_area,
+                              "CH4_flux_umol_m2_h" = ch4_flux*3600)
+        
+      } else {
+        
+        lm_model_co2 <- lm(co2~sec, data = data_subset)
+        slope_co2 <- coef(lm_model_co2)[2]
+        intercept_co2 <- coef(lm_model_co2)[1]
+        r2_co2 <- summary(lm_model_co2)$r.squared
+        
+        co2_flux <- (slope_co2*(input$chamber_vol*input$atm_pres))/(R*(273.15+mean_temp)*input$chamber_area)
+      
+        results_string <- paste0("<b>CH<sub>4</sub></b>: slope = ", round(slope_ch4*3600, 2), " (ppm h<sup>-1</sup>)",
                                ", flux = ", round(ch4_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)",
                                ", R<sup>2</sup> = ", round(r2_ch4, 2),
                                "<br>", 
@@ -225,7 +265,7 @@ server <- function(input, output, session){
                                ", flux = ", round(co2_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)", 
                                ", R<sup>2</sup> = ", round(r2_co2, 2))
       
-      results <- data.frame("processing_date" = strftime(Sys.time(), "%Y-%m-%d %H:%M:%S", tz="UTC"),
+        results <- data.frame("processing_date" = strftime(Sys.time(), "%Y-%m-%d %H:%M:%S", tz="UTC"),
                             "id" = as.character(input$sample_id),
                             "start" = strftime(ranges2$x[1], "%Y-%m-%d %H:%M:%S", tz="UTC"),
                             "end" = strftime(ranges2$x[2], "%Y-%m-%d %H:%M:%S", tz="UTC"),
@@ -240,7 +280,7 @@ server <- function(input, output, session){
                             "chamber_area" = input$chamber_area,
                             "CH4_flux_umol_m2_h" = ch4_flux*3600,
                             "CO2_flux_umol_m2_h" = co2_flux*3600)
-      
+      }  
     }else{
       data_subset <- data() %>% 
         mutate(sec = cumsum(c(0, diff(as.numeric(datetime))))) 
@@ -251,7 +291,8 @@ server <- function(input, output, session){
     
     return(list("df" = data_subset, 
                 "results" = results, 
-                "results_string" = results_string))
+                "results_string" = results_string,
+                "co2_status" = co2_status))
     
   })
   
@@ -268,35 +309,50 @@ server <- function(input, output, session){
          main= "Zoom plot",
          col = "darkorange")
     
-    mtext(expression("CO"[2]*" (ppm)"), side = 4, line = 3, col="forestgreen")
-    
-    co2_min = min(zoom_data$df$co2)
-    co2_max = max(zoom_data$df$co2)
     water_min = min(zoom_data$df$water)
     ch4_min = min(zoom_data$df$ch4)
     ch4_max = max(zoom_data$df$ch4)
     water_max = max(zoom_data$df$water)
     water_scaled = (ch4_max - ch4_min)*((zoom_data$df$water-water_min)/(water_max - water_min))+ch4_min
-    
     points(x = zoom_data$df$sec, y = water_scaled, col="lightblue", type="l")
     
-    ch4_scaled = (co2_max - co2_min)*((zoom_data$df$ch4-ch4_min)/(ch4_max - ch4_min))+co2_min
-    ch4_labels = pretty(zoom_data$df$ch4)
-    ch4_at = (co2_max - co2_min)*((ch4_labels-ch4_min)/(ch4_max - ch4_min))+co2_min
-    
-    co2_scaled = (ch4_max - ch4_min)*((zoom_data$df$co2-co2_min)/(co2_max - co2_min))+ch4_min
-    co2_labels = pretty(zoom_data$df$co2)
-    co2_at = (ch4_max - ch4_min)*((co2_labels-co2_min)/(co2_max - co2_min))+ch4_min
-    
-    
-    
-    points(x = zoom_data$df$sec, y = co2_scaled, col="forestgreen")
-    axis(4, at = co2_at, labels = co2_labels, col="forestgreen", col.ticks="forestgreen")
-    
-    if (!is.null(ranges2$x)){
+    if (zoom_data$co2_status$co2_mean == 0 & zoom_data$co2_status$co2_var == 0) {
+      if (!is.null(ranges2$x)){
+        
+        output$result_string <- renderText(zoom_data$results_string)
+      }
+        
+        abline(zoom_data$results$CH4_intercept,
+               zoom_data$results$CH4_slope/3600,
+               col = "darkorange", lwd = 4)
       
-      output$result_string <- renderText(zoom_data$results_string)
+        
       
+      legend("topright", 
+             c(expression("CH"[4]), expression("H"[2]*"O")), 
+             col = c("darkorange", "lightblue"), pch=19)
+      
+      } else {
+      
+      mtext(expression("CO"[2]*" (ppm)"), side = 4, line = 3, col="forestgreen")
+      co2_min = min(zoom_data$df$co2)
+      co2_max = max(zoom_data$df$co2)
+        
+      co2_scaled = (ch4_max - ch4_min)*((zoom_data$df$co2-co2_min)/(co2_max - co2_min))+ch4_min
+      co2_labels = pretty(zoom_data$df$co2)
+      co2_at = (ch4_max - ch4_min)*((co2_labels-co2_min)/(co2_max - co2_min))+ch4_min
+     
+      ch4_scaled = (co2_max - co2_min)*((zoom_data$df$ch4-ch4_min)/(ch4_max - ch4_min))+co2_min
+      ch4_labels = pretty(zoom_data$df$ch4)
+      ch4_at = (co2_max - co2_min)*((ch4_labels-ch4_min)/(ch4_max - ch4_min))+co2_min
+      
+      points(x = zoom_data$df$sec, y = co2_scaled, col="forestgreen")
+      axis(4, at = co2_at, labels = co2_labels, col="forestgreen", col.ticks="forestgreen") 
+      
+      if (!is.null(ranges2$x)){
+        
+        output$result_string <- renderText(zoom_data$results_string)
+      }
       
       abline(zoom_data$results$CH4_intercept,
              zoom_data$results$CH4_slope/3600,
@@ -309,12 +365,13 @@ server <- function(input, output, session){
       abline(intercept_co2_scaled,
              slope_co2_scaled,
              col = "forestgreen", lwd = 4)
-    }
+  
     
-    legend("topright", 
+      legend("topright", 
            c(expression("CH"[4]),expression("CO"[2]), expression("H"[2]*"O")), 
            col = c("darkorange", "forestgreen", "lightblue"), pch=19)
     
+      }
   })
   
   observe({
