08be968e95e97dd961ae25786c029c77a4fc9a50
JonasStage <Jonasstage@hotmail.com>
2024-07-10 09:58:08 +0200

    Added posibility to change minimium and maximum CH4 and CO2 values

--- app.R
+++ app.R
@@ -34,41 +34,47 @@ ui <- fluidPage(
       
       tags$hr(),
       
-      tags$b("Timezone of measurement"), selectizeInput("timezone", NULL, width="100%",  
-                                           choices = list("GMT-12"="GMT-12",
-                                                          "GMT-11"="GMT-11",
-                                                          "GMT-10"="GMT-10",
-                                                          "GMT-9"="GMT-9",
-                                                          "GMT-8"="GMT-8",
-                                                          "GMT-7"="GMT-7",
-                                                          "GMT-6"="GMT-6",
-                                                          "GMT-5"="GMT-5",
-                                                          "GMT-4"="GMT-4",
-                                                          "GMT-3"="GMT-3",
-                                                          "GMT-2"="GMT-2",
-                                                          "GMT-1"="GMT-1",
-                                                          "GMT"="GMT",
-                                                          "GMT+1"="GMT+1",
-                                                          "GMT+2"="GMT+2",
-                                                          "GMT+3"="GMT+3",
-                                                          "GMT+4"="GMT+4",
-                                                          "GMT+5"="GMT+5",
-                                                          "GMT+6"="GMT+6",
-                                                          "GMT+7"="GMT+7",
-                                                          "GMT+8"="GMT+8",
-                                                          "GMT+9"="GMT+9",
-                                                          "GMT+10"="GMT+10",
-                                                          "GMT+11"="GMT+11",
-                                                          "GMT+12"="GMT+12")),
+      tags$b("Adjust methane range"),
+      
+      sliderInput("ch4_range", "",
+                  min = 0, max = 10000, value = c(0, 10000)),
+      
       tags$hr(),
       
-      tags$b("Enter metadata"),
+      tags$b("Adjust CO2 range"),
       
-      tags$p("Chamber volume (L):"), numericInput("chamber_vol", NULL, 10, min = 0, width = "100px"),
+      sliderInput("co2_range", "",
+                  min = 0, max = 10000, value = c(0, 10000)),
       
-      tags$p("Chamber area (m2):"), numericInput("chamber_area", NULL, 0.5, min = 0, width = "100px"),
+      tags$hr(),
       
-      tags$p("Atmos. pressure (atm):"), numericInput("atm_pres", NULL, 1, min = 0, width = "100px"),
+      tags$b("Timezone of measurement"), selectizeInput("timezone", NULL, width="100%",  
+                                                        choices = list("GMT-12"="GMT-12",
+                                                                       "GMT-11"="GMT-11",
+                                                                       "GMT-10"="GMT-10",
+                                                                       "GMT-9"="GMT-9",
+                                                                       "GMT-8"="GMT-8",
+                                                                       "GMT-7"="GMT-7",
+                                                                       "GMT-6"="GMT-6",
+                                                                       "GMT-5"="GMT-5",
+                                                                       "GMT-4"="GMT-4",
+                                                                       "GMT-3"="GMT-3",
+                                                                       "GMT-2"="GMT-2",
+                                                                       "GMT-1"="GMT-1",
+                                                                       "GMT"="GMT",
+                                                                       "GMT+1"="GMT+1",
+                                                                       "GMT+2"="GMT+2",
+                                                                       "GMT+3"="GMT+3",
+                                                                       "GMT+4"="GMT+4",
+                                                                       "GMT+5"="GMT+5",
+                                                                       "GMT+6"="GMT+6",
+                                                                       "GMT+7"="GMT+7",
+                                                                       "GMT+8"="GMT+8",
+                                                                       "GMT+9"="GMT+9",
+                                                                       "GMT+10"="GMT+10",
+                                                                       "GMT+11"="GMT+11",
+                                                                       "GMT+12"="GMT+12")),
+      tags$hr(),
       
       tags$b("Adjust time range"),
       
@@ -81,6 +87,14 @@ ui <- fluidPage(
                   timezone="+0000"),
       
       tags$hr(),
+      
+      tags$b("Enter metadata"),
+      
+      tags$p("Chamber volume (L):"), numericInput("chamber_vol", NULL, 10, min = 0, width = "100px"),
+      
+      tags$p("Chamber area (m2):"), numericInput("chamber_area", NULL, 0.5, min = 0, width = "100px"),
+      
+      tags$p("Atmos. pressure (atm):"), numericInput("atm_pres", NULL, 1, min = 0, width = "100px"),
       
       tags$b("Select and save flux (repeat)"),
       
@@ -207,11 +221,25 @@ server <- function(input, output, session){
     time_start <- min(df$datetime)
     time_end <- max(df$datetime)
     
+    ch4_start <- floor(min(df$ch4, na.rm=T))
+    ch4_end <- ceiling(max(df$ch4, na.rm=T))
+    
+    co2_start <- floor(min(df$co2))
+    co2_end <- ceiling(max(df$co2))
+    
     updateSliderInput(session, "range", value = c(time_start, time_end),
                       min = time_start, max = time_end, step = 60)
     
+    updateSliderInput(session, "ch4_range", value = c(ch4_start, ch4_end),
+                      min = ch4_start, max = ch4_end, step = 10)
+    
+    updateSliderInput(session, "co2_range", value = c(co2_start, co2_end),
+                      min = co2_start, max = co2_end, step = 100)
+    
+    
+    
     return(df)
-
+    
     
   })
   
@@ -220,7 +248,9 @@ server <- function(input, output, session){
   output$plot <- renderPlot({
     
     data_subset <- data() %>% 
-      filter(between(datetime, input$range[1], input$range[2]))
+      filter(between(datetime, input$range[1], input$range[2]),
+             between(ch4, input$ch4_range[1],input$ch4_range[2]),
+             between(co2, input$co2_range[1],input$co2_range[2]),)
     
     #par(mar = c(5,4,4,4) + 0.1)
     
@@ -231,8 +261,8 @@ server <- function(input, output, session){
     #      main = "Overview plot",
     #      col = "darkorange")
     
-     data() %>% 
-        reframe(across(co2, c(mean = mean,var = var))) -> co2_status
+    data() %>% 
+      reframe(across(co2, c(mean = mean,var = var))) -> co2_status
     
     ggplot() + 
       geom_point(data = data_subset, aes(datetime, ch4, col = "CH4")) + 
@@ -247,16 +277,16 @@ server <- function(input, output, session){
                        date_labels = "%R") + 
       theme_bw() + 
       theme(legend.position = "bottom") -> op1
-     
+    
     if (co2_status$co2_mean == 0 & co2_status$co2_var == 0) {
       # legend("topright", 
       #        c(expression("CH"[4])), 
       #        col = "darkorange", pch=19)
       op1
-      } else {
-    
+    } else {
+      
       #mtext(expression("CO"[2]*" (ppm)"), side = 4, line = 3, col="forestgreen")
-  
+      
       co2_min = min(data_subset$co2)
       co2_max = max(data_subset$co2)
       ch4_min = min(data_subset$ch4)
@@ -290,7 +320,7 @@ server <- function(input, output, session){
         scale_x_datetime(date_breaks = "10 min",
                          date_minor_breaks = "1 min",
                          date_labels = "%R")
-     }
+    }
     
   })
   
@@ -298,9 +328,9 @@ server <- function(input, output, session){
   
   data_subset <- reactive({
     req(input$file)
-
+    
     data() %>% 
-        reframe(across(co2, c(mean = mean,var = var))) -> co2_status
+      reframe(across(co2, c(mean = mean,var = var))) -> co2_status
     
     if (!is.null(ranges2$x)) {
       ranges2$x <- as_datetime(ranges2$x)
@@ -321,10 +351,10 @@ server <- function(input, output, session){
       
       ch4_flux <- (slope_ch4*(input$chamber_vol*input$atm_pres))/(R*(273.15+mean_temp)*input$chamber_area)
       
-        # legend("topright", 
-        #        c(expression("CH"[4])), 
-        #        col = "darkorange", pch=19)
-     
+      # legend("topright", 
+      #        c(expression("CH"[4])), 
+      #        col = "darkorange", pch=19)
+      
       if (co2_status$co2_mean == 0 & co2_status$co2_var == 0) {
         
         
@@ -352,30 +382,30 @@ server <- function(input, output, session){
         r2_co2 <- summary(lm_model_co2)$r.squared
         
         co2_flux <- (slope_co2*(input$chamber_vol*input$atm_pres))/(R*(273.15+mean_temp)*input$chamber_area)
-      
+        
         results_string <- paste0("<b>CH<sub>4</sub></b>: slope = ", round(slope_ch4*3600, 2), " (ppm h<sup>-1</sup>)",
-                               ", flux = ", round(ch4_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)",
-                               ", R<sup>2</sup> = ", round(r2_ch4, 2),
-                               "<br>", 
-                               "<b>CO<sub>2</sub></b>: slope = ", round(slope_co2*3600, 2), " (ppm h<sup>-1</sup>)", 
-                               ", flux = ", round(co2_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)", 
-                               ", R<sup>2</sup> = ", round(r2_co2, 2))
-      
+                                 ", flux = ", round(ch4_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)",
+                                 ", R<sup>2</sup> = ", round(r2_ch4, 2),
+                                 "<br>", 
+                                 "<b>CO<sub>2</sub></b>: slope = ", round(slope_co2*3600, 2), " (ppm h<sup>-1</sup>)", 
+                                 ", flux = ", round(co2_flux*3600, 2), " (µmol m<sup>-2</sup> h<sup>-1</sup>)", 
+                                 ", R<sup>2</sup> = ", round(r2_co2, 2))
+        
         results <- data.frame("processing_date" = strftime(Sys.time(), "%Y-%m-%d %H:%M:%S", tz="GMT"),
-                            "id" = as.character(input$sample_id),
-                            "start" = strftime(ranges2$x[1], "%Y-%m-%d %H:%M:%S", tz="GMT"),
-                            "end" = strftime(ranges2$x[2], "%Y-%m-%d %H:%M:%S", tz="GMT"),
-                            "CH4_slope" = slope_ch4*3600,
-                            "CH4_intercept" = intercept_ch4,
-                            "CH4_R2" = r2_ch4,
-                            "CO2_slope" = slope_co2*3600,
-                            "CO2_intercept" = intercept_co2,
-                            "CO2_R2" = r2_co2,
-                            "temperature" = mean_temp, 
-                            "chamber_volume" = input$chamber_vol,
-                            "chamber_area" = input$chamber_area,
-                            "CH4_flux_umol_m2_h" = ch4_flux*3600,
-                            "CO2_flux_umol_m2_h" = co2_flux*3600)
+                              "id" = as.character(input$sample_id),
+                              "start" = strftime(ranges2$x[1], "%Y-%m-%d %H:%M:%S", tz="GMT"),
+                              "end" = strftime(ranges2$x[2], "%Y-%m-%d %H:%M:%S", tz="GMT"),
+                              "CH4_slope" = slope_ch4*3600,
+                              "CH4_intercept" = intercept_ch4,
+                              "CH4_R2" = r2_ch4,
+                              "CO2_slope" = slope_co2*3600,
+                              "CO2_intercept" = intercept_co2,
+                              "CO2_R2" = r2_co2,
+                              "temperature" = mean_temp, 
+                              "chamber_volume" = input$chamber_vol,
+                              "chamber_area" = input$chamber_area,
+                              "CH4_flux_umol_m2_h" = ch4_flux*3600,
+                              "CO2_flux_umol_m2_h" = co2_flux*3600)
       }  
     }else{
       data_subset <- data() %>% 
@@ -412,7 +442,7 @@ server <- function(input, output, session){
     water_max = max(zoom_data$df$water)
     water_scaled = (ch4_max - ch4_min)*((zoom_data$df$water-water_min)/(water_max - water_min))+ch4_min
     # points(x = zoom_data$df$sec, y = water_scaled, col="lightblue", type="l")
-
+    
     ggplot() + 
       geom_point(data = zoom_data$df, aes(sec, ch4, col = "CH4")) +
       geom_smooth(data = zoom_data$df, aes(sec, ch4, col = "CH4"), method = "lm", se = F) +
@@ -431,27 +461,27 @@ server <- function(input, output, session){
         
         output$result_string <- renderText(zoom_data$results_string)
       }
-        
-        # abline(zoom_data$results$CH4_intercept,
-        #        zoom_data$results$CH4_slope/3600,
-        #        col = "darkorange", lwd = 4)
       
-        
+      # abline(zoom_data$results$CH4_intercept,
+      #        zoom_data$results$CH4_slope/3600,
+      #        col = "darkorange", lwd = 4)
+      
+      
       # 
       # legend("topright", 
       #        c(expression("CH"[4]), expression("H"[2]*"O")), 
       #        col = c("darkorange", "lightblue"), pch=19)
       
-      } else {
+    } else {
       
       # mtext(expression("CO"[2]*" (ppm)"), side = 4, line = 3, col="forestgreen")
       co2_min = min(zoom_data$df$co2)
       co2_max = max(zoom_data$df$co2)
-        
+      
       co2_scaled = (ch4_max - ch4_min)*((zoom_data$df$co2-co2_min)/(co2_max - co2_min))+ch4_min
       co2_labels = pretty(zoom_data$df$co2)
       co2_at = (ch4_max - ch4_min)*((co2_labels-co2_min)/(co2_max - co2_min))+ch4_min
-     
+      
       ch4_scaled = (co2_max - co2_min)*((zoom_data$df$ch4-ch4_min)/(ch4_max - ch4_min))+co2_min
       ch4_labels = pretty(zoom_data$df$ch4)
       ch4_at = (co2_max - co2_min)*((ch4_labels-ch4_min)/(ch4_max - ch4_min))+co2_min
@@ -475,8 +505,8 @@ server <- function(input, output, session){
       # abline(intercept_co2_scaled,
       #        slope_co2_scaled,
       #        col = "forestgreen", lwd = 4)
-  
-    
+      
+      
       # legend("topright", 
       #      c(expression("CH"[4]),expression("CO"[2]), expression("H"[2]*"O")), 
       #      col = c("darkorange", "forestgreen", "lightblue"), pch=19)
@@ -490,7 +520,7 @@ server <- function(input, output, session){
                            values = c("darkorange","forestgreen", "lightblue")) +
         scale_y_continuous(sec.axis = sec_axis(trans=~., name = bquote("CO"[2]*" (ppm)"),
                                                breaks = co2_at, labels = co2_labels)) 
-      }
+    }
   })
   
   observe({
@@ -522,7 +552,7 @@ server <- function(input, output, session){
     content = function(file) {
       write.csv(data_out, file, row.names = FALSE)
     })  
-
+  
   output$ch4_download <- downloadHandler(
     
     filename = function() {
@@ -532,7 +562,7 @@ server <- function(input, output, session){
     content = function(file) {
       write.csv(data(), file, row.names = FALSE)
     })  
-    
+  
 }
 
 shinyApp(ui, server)
